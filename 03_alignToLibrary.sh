bd23f7e84282fdc694211f8a7730ea8fefcecebc
Nathan Abell <nsabell@stanford.edu>
2017-04-10 23:19:18 -0700

    postprocessing updates

--- 03_alignToLibrary.sh
+++ 03_alignToLibrary.sh
@@ -5,25 +5,25 @@
 # Stanford University
 # SCG4
 
-#module load bwa/0.7.15
+module load bwa/0.7.15
 module load samtools/0.1.19
 
 cd /home/nsabell/scratch/mpra
-#mkdir bam
+mkdir bam
 
-#bwa mem -L 100 -k 8 -O 5 -t 6 reference/bwa/1KG/1KG fastq_pairMerged/1KG_pairMerged_processed.fastq.gz > bam/1KG_pairMerged.sam &
+bwa mem -L 100 -k 8 -O 5 -t 6 reference/bwa/1KG/1KG fastq_pairMerged/1KG_pairMerged_processed.fastq.gz > bam/1KG_pairMerged.sam &
 
-#bwa mem -L 100 -k 8 -O 5 -t 6 reference/bwa/GTEX/GTEX fastq_pairMerged/GTEX_pairMerged_processed.fastq.gz > bam/GTEX_pairMerged.sam &
+bwa mem -L 100 -k 8 -O 5 -t 6 reference/bwa/GTEX/GTEX fastq_pairMerged/GTEX_pairMerged_processed.fastq.gz > bam/GTEX_pairMerged.sam &
 
-#wait
+wait
 
-#samtools view -bS bam/1KG_pairMerged.sam > bam/1KG_pairMerged.bam &
-#samtools view -bS bam/GTEX_pairMerged.sam > bam/GTEX_pairMerged.bam &
+samtools view -bS bam/1KG_pairMerged.sam > bam/1KG_pairMerged.bam &
+samtools view -bS bam/GTEX_pairMerged.sam > bam/GTEX_pairMerged.bam &
 
-#wait
+wait
 
-samtools sort -@ 6 -m 2G bam/GTEX_pairMerged.bam bam/GTEX_pairMerged_sorted &
-samtools sort -@ 6 -m 2G bam/1KG_pairMerged.bam bam/1KG_pairMerged_sorted &
+samtools sort -@ 4 bam/GTEX_pairMerged.bam bam/GTEX_pairMerged_sorted &
+samtools sort -@ 4 bam/1KG_pairMerged.bam bam/1KG_pairMerged_sorted &
 
 wait
 
@@ -31,3 +31,6 @@ samtools index bam/1KG_pairMerged_sorted.bam &
 samtools index bam/GTEX_pairMerged_sorted.bam &
 
 wait
+
+rm bam/1KG_pairMerged.sam bam/GTEX_pairMerged.sam bam/1KG_pairMerged.bam bam/GTEX_pairMerged.bam
+
---
+++ recover_barcode_oligo_map.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Nathan Abell
+# Montgomery Lab
+# Stanford University
+# Durga
+
+# Recover oligo->barcode mappings
+
+# Still considering which fields are critical to extract
+# Matrix will have ~100 million rows so important decision 
