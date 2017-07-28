4930101e2a1dbb7f8ee87899460bd32fc25baa73
nsabell <abell.nathan@gmail.com>
2017-07-27 17:48:07 -0700

    running pipe

--- 01_readPairProcessing.sh
+++ 01_readPairProcessing.sh
@@ -14,25 +14,14 @@ OUTDIR=`dirname $3`
 
 module load fastx_toolkit
 cd /home/nsabell/scratch/mpra
-export FLASH=$PATH:/home/nsabell/scratch/mpra/bin/FLASH-1.2.11/flash
+export FLASH=/home/nsabell/scratch/mpra/bin/FLASH-1.2.11/flash
 mkdir -p $OUTDIR
 
 # Merge 1KG samples with Flash
-$FLASH -z -r 150 -f 223 -s 10 fastq/run1/1KG_R1.fastq.gz fastq/run1/1KG_R2.fastq.gz
-mv out.extendedFrags.fastq.gz fastq_pairMerged/run1/1KG_pairMerged.fastq.gz
-mv out.notCombined_1.fastq.gz fastq_pairMerged/run1/1KG_R1_nonmerged.fastq.gz
-mv out.notCombined_2.fastq.gz fastq_pairMerged/run1/1KG_R2_nonmerged.fastq.gz
-mv out.hist fastq_pairMerged/run1/1KG_out.hist
-mv out.histogram fastq_pairMerged/run1/1KG_out.histogram
-
+$FLASH -o $OUTPFX -z -r 150 -f 223 -s 10 $R1 $R2
 mv *.o* *.e* logs
 
 ## Extract barcodes and collapse to get frequency
-gunzip -c fastq_pairMerged/run1/1KG_pairMerged.fastq.gz | fastx_trimmer -l 20 | fastq_to_fasta -n -r | fastx_collapser | gzip > uniqBarcodes/1KG_uniqBarcodes.fastq.gz
-gunzip -c fastq_pairMerged/run2/1KG_pairMerged.fastq.gz | fastx_trimmer -l 20 | fastq_to_fasta -n -r | fastx_collapser | gzip > uniqBarcodes/1KG_uniqBarcodes_combined.fastq.gz
-gunzip -c fastq_pairMerged/GTEX_pairMerged.fastq.gz | fastx_trimmer -l 20 | fastq_to_fasta -n -r | fastx_collapser | gzip > uniqBarcodes/GTEX_uniqBarcodes.fastq.gz
-
-
-
+gunzip -c ${OUTPFX}_pairMerged.fastq.gz | fastx_trimmer -l 20 | fastq_to_fasta -n -r | fastx_collapser | gzip > ${OUTPFX}_uniqBarcodes.fastq.gz
 
 
